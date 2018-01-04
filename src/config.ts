dbd38d64b8093354cf4b6c1ccb7f44d1fd13a946
f0reachARR <f0reach@f0reach.me>
2018-01-05 00:05:20 +0900

    Endpoint追加&変更

--- src/config.ts
+++ src/config.ts
@@ -10,11 +10,12 @@ interface Config {
     db: string;
 }
 
-export const LINE_APP = 'CHROMEOS\t1.4.10\tChrome_OS\t1';
+export const LINE_APP = 'CHROMEOS\t1.4.17\tChrome_OS\t1';
 export const LINE_UNAUTH_ENDPOINT = 'https://ga2.line.naver.jp/api/v4/TalkService.do';
-export const LINE_AUTH_ENDPOINT = 'https://ga2.line.naver.jp/S4';
+export const LINE_NORMAL_ENDPOINT = 'https://ga2.line.naver.jp/S4';
 export const LINE_POLL_ENDPOINT = 'https://ga2.line.naver.jp/P4';
+export const LINE_AUTH_ENDPOINT = 'https://ga2.line.naver.jp/api/v4p/rs';
 export const LINE_QR_POLL = 'https://ga2.line.naver.jp/Q';
-export const LINE_SYSTEM_NAME = 'Chrome PC';
+export const LINE_SYSTEM_NAME = 'Chrome';
 export const E2EE_VERSION = 1;
 export default (() => safeLoad(readFileSync('config.yml', 'utf-8')))() as Config;
