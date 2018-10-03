34e63d02622af5ac8a5edf04733fe0998df74023
f0reachARR <f0reach@f0reach.me>
2018-10-03 13:51:41 +0900

    dotenvでの設定読み込み

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,7 @@
 import { safeLoad } from 'js-yaml';
 import { readFileSync } from 'fs';
-import * as assert from 'assert';
-
-interface Config {
-    slack: {
-        token: string;
-        appToken: string;
-        user: string;
-    };
-    db: string;
-}
+import * as joi from 'joi';
+import * as dotenv from 'dotenv';
 
 export const LINE_APP = 'CHROMEOS\t1.4.17\tChrome_OS\t1';
 export const LINE_UNAUTH_ENDPOINT = 'https://ga2.line.naver.jp/api/v4/TalkService.do';
@@ -20,20 +12,35 @@ export const LINE_QR_POLL = 'https://ga2.line.naver.jp/Q';
 export const LINE_SYSTEM_NAME = 'Chrome';
 export const E2EE_VERSION = 1;
 
-const config: Config = process.env.SLACK_TOKEN ? {
+dotenv.config();
+
+interface Config {
     slack: {
-        token: process.env.SLACK_TOKEN || '',
-        appToken: process.env.SLACK_APP_TOKEN || '',
-        user: process.env.SLACK_USER || ''
+        token: string;
+        appToken: string;
+        user: string;
+    };
+    db: string;
+}
+
+const configScheme = joi.object().keys({
+    SLACK_TOKEN: joi.string().required(),
+    SLACK_APP_TOKEN: joi.string().required(),
+    SLACK_USER: joi.string().required(),
+    DATABASE: joi.string().uri().required()
+}).unknown(true);
+
+const { error: validateError } = configScheme.validate(process.env);
+if (validateError) {
+    console.error(validateError);
+    process.exit(-1);
+}
+
+export default {
+    slack: {
+        token: process.env.SLACK_TOKEN,
+        appToken: process.env.SLACK_APP_TOKEN,
+        user: process.env.SLACK_USER
     },
-    db: process.env.DATABASE || ''
-} : (() => safeLoad(readFileSync('config.yml', 'utf-8')))() as Config;
-
-assert(config);
-assert(config.slack);
-assert(!!config.slack.token);
-assert(!!config.slack.appToken);
-assert(!!config.slack.user);
-assert(!!config.db);
-
-export default config;
+    db: process.env.DATABASE
+} as Config;
