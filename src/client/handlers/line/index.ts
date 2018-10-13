c9207036e7baf4517260c0be4723a83b7037a4ac
f0reachARR <f0reach@f0reach.me>
2018-10-13 15:30:41 +0900

    E2EE handlerをMessage handlerに統合したので削除

--- src/client/handlers/line/index.ts
+++ src/client/handlers/line/index.ts
@@ -1,9 +1,7 @@
 import { LineHandler } from '../..';
 
-import e2eeHandler from './e2ee';
 import messageHandler from './message';
 
 export const handlers: LineHandler[] = [
-    e2eeHandler,
     messageHandler
 ];
